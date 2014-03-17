66336f394d79901b3942c300b31b189ccf4fbdce
cchan <dincoco@gmail.com>
2014-03-16 20:55:44 -0400

    ApplicationService
    Haven't tested yet.
    All other services should be similar to this one.

--- project/JobPortalv1/src/service/ApplicationService.java
+++ project/JobPortalv1/src/service/ApplicationService.java
@@ -1,11 +1,14 @@
 package service;
 
+import java.util.Calendar;
+
 import util.SessionCtl;
 import modelMB.ApplicationBean;
 import modelMB.AssetBean;
 import modelMB.JobBean;
 import modelMB.UserBean;
 import data.dao.ApplicationDAO;
+import data.dao.ApplicationStatusDAO;
 import data.dao.AssetDAO;
 import data.dao.JobDAO;
 import data.dao.UserDAO;
@@ -40,7 +43,7 @@ public class ApplicationService {
 		loadFromEntity(appBean, app);
 	}
 
-	public static void saveOrUpdate(ApplicationBean appBean) {
+	public static int saveOrUpdate(ApplicationBean appBean) {
 		Application app = null;
 		Integer id = appBean.getId();
 
@@ -52,6 +55,7 @@ public class ApplicationService {
 			app = new Application(SessionCtl.getLoggedInUser().getUser_name());
 		}
 
+		// Fetch all necessary object from database
 		// Copy new data from bean to entity
 		UserDAO userDao = new UserDAO();
 		User user = userDao.getEntityById(appBean.getApplicant().getUser_id());
@@ -63,7 +67,20 @@ public class ApplicationService {
 		JobDAO jobDao = new JobDAO();
 		Job job = jobDao.getEntityById(appBean.getJob().getId());
 		
-		ApplicationStatus status;
-		appBean.getStatus();
+		ApplicationStatusDAO appStatusDao = new ApplicationStatusDAO();
+		ApplicationStatus status = appStatusDao.getByName(appBean.getStatus());
+		
+		int result = -1;
+		if ( user != null && asset != null && job != null && status != null) {
+			app.setApplicant(user);
+			app.setAsset(asset);
+			app.setPosition(job);
+			app.setStatus(status);
+			app.setUpdate_user(SessionCtl.getLoggedInUser().getUser_name());
+			app.setUpdate_time(Calendar.getInstance());
+			applicationDao.saveOrUpdate(app);
+		}
+		
+		return result;
 	}
 }
